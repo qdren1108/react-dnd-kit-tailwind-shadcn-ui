055461498463e1a75361fe789f2fa464542ee355
qdren <610126181@qq.com>
2025-04-14 16:12:11 +0800

    1

--- src/components/BoardColumn.tsx
+++ src/components/BoardColumn.tsx
@@ -2,6 +2,7 @@ import { SortableContext, useSortable } from "@dnd-kit/sortable";
 import { useDndContext, type UniqueIdentifier } from "@dnd-kit/core";
 import { CSS } from "@dnd-kit/utilities";
 import { useMemo } from "react";
+import React from "react";
 import { Task, TaskCard } from "./TaskCard";
 import { cva } from "class-variance-authority";
 import { Card, CardContent, CardHeader } from "./ui/card";
